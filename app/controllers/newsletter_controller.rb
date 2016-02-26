99d579fe9a2fcd8283cfea0ae6622e44cc1aa186
Jack Higgins <jackhiggins07@gmail.com>
2016-02-26 22:23:04 +0000

    Mailchimp and Fakechimp helpers

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,5 +1,19 @@
 class NewsletterController < ApplicationController
+  attr_accessor :mailchimp, :campaign_list
+
+  def initialize
+    begin
+      @mailchimp = NewsletterHelper::MailchimpHelper.new
+    rescue Mailchimp::Error => e
+      Rails.logger.error e.message
+      @mailchimp = NewsletterHelper::FakechimpHelper.new
+    end
+
+    super
+  end
+
   def index
+    @campaign_list = @mailchimp.get_campaign_list
   end
 
   def get_campaign
--- app/helpers/newsletter_helper.rb
+++ app/helpers/newsletter_helper.rb
@@ -1,2 +1,38 @@
+require 'json'
+require 'mailchimp'
+require 'mailchimp/errors'
+
 module NewsletterHelper
+  class MailchimpHelper
+    attr_accessor :mailchimp
+
+    def initialize
+      @mailchimp = Mailchimp::API.new Rails.application.secrets.mailchimp_api_key
+    end
+
+    def get_campaign_html(campaign_id)
+      @mailchimp.campaigns.content(campaign_id)['html']
+    end
+
+    # Returns an array of emails sent
+    # Emails are stored in a hash
+    def get_campaign_list
+      filters = {status: 'sent'}
+      @mailchimp.campaigns.list(filters)['data']
+    end
+  end
+
+  # Fakechimp is used when a valid Mailchimp API Key can not be found
+  class FakechimpHelper
+    def get_campaign_html(campaign_id)
+      File.read 'data/fake-data/mailchimp-campaigns-BigUpdate-content.html'
+    end
+
+    # Returns an array of emails sent
+    # Emails are stored in a hash
+    def get_campaign_list
+      fake_data = JSON.parse(File.read 'data/fake-data/mailchimp-campaigns.json')
+      fake_data['data']
+    end
+  end
 end
