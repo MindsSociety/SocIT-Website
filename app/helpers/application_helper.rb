3897e587b9a8091d44cb66f6be4528c49ccd53f5
Jack Higgins <jackhiggins07@gmail.com>
2016-02-27 13:39:59 +0000

    Navbar now supports screen readers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Bootstrap navbar
   def nav_bar
-    content_tag(:nav, class: "nav navbar-nav") do
+    content_tag :nav, class: "nav navbar-nav" do
       yield
     end
   end
@@ -10,14 +10,18 @@ module ApplicationHelper
   # Extension of 'link_to' for bootstrap
   # Sets "class='active'" in the <li> tag"
   def nav_link(text, path)
-    if current_page?(path)
-      options = { class: 'active' }
-    else
-      options = {}
-    end
+    if current_page? path
+      content_tag :li, class: 'active'  do
 
-    content_tag(:li, options) do
-      link_to text, path
+        # <span class="sr-only">(current)</span> required for Screen Readers
+        link_to path do
+          sanitize text + content_tag(:span, '(current)', class: 'sr-only')
+        end
+      end
+    else
+      content_tag :li do
+        link_to(text, path)
+      end
     end
   end
 end
