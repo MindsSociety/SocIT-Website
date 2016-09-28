749c4ed7bb0a3ce77176aa77d1c5d1e7557a1cd8
Jack Higgins <jackhiggins07@gmail.com>
2016-09-28 23:04:45 +0100

    Deployed to SocIT servers

---
+++ config/puma.rb
@@ -0,0 +1,22 @@
+rails_env = 'production'
+environment rails_env
+port ENV.fetch("PORT", 80)
+
+# daemonize
+pidfile "#{Dir.pwd}/tmp/pids/puma.pid"
+state_path "#{Dir.pwd}/tmp/pids/puma.pid.state"
+
+# Change workers to match CPU core count
+workers 8
+threads 0, 16
+
+# bind "unix://#{Dir.pwd}/tmp/sockets/puma.sock"
+
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{Dir.pwd}/config/database.yml")[rails_env])
+end
+
