7cc51ff784e7ccd0357ab713296b4423d68cccce
Jack Higgins <jackhiggins07@gmail.com>
2017-05-30 22:38:08 +0100

    username can be null

--- db/migrate/20170422143401_devise_create_users.rb
+++ db/migrate/20170422143401_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :username, null: false, unique: true
+      t.string :username,         unique: true
       t.integer :student_number
       t.string :phone_number
       t.boolean :email_opt_in,    default: false
