c9dd96e6a67c252cd3a503ae286ee934708f2f91
Jack Higgins <jackhiggins07@gmail.com>
2017-07-04 18:04:05 +0100

    Moved MailChimp sign up form to separate template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'newsletter/:campaign_id', to: 'newsletter#get_campaign'
 
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users
+  resources :users, only: [:destroy, :edit, :show, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
